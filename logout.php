d5704333a131638dc6b0cee4b7c8a40e7400df39
aekkakti <music.tomsk1@gmail.com>
2024-03-28 10:55:58 +0700

    Логаут тоже чутка изменил

--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php
-session_start();
-if(session_destroy()) {
-    header("Location: login.php");
-}
+require_once 'connected.php';
+setcookie('username', null);
+header('Location: login.php');
 ?>
